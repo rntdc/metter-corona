b14cafc4c884dde09bee13832c4074214bbb0ff5
Renato <rntdek@gmaail.com>
2020-03-24 14:44:29 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const ejs = require('ejs');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.get('/', function(req, res) {
+    res.render('/home');
+});
+
+app.listen(7000, function (err) {
+    if(err){
+        console.log('Erro na inicializacao')
+    } else{
+        console.log('OK!');
+    }
+})
